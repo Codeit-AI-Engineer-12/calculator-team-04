3f06249ebf0e79f957591de2ed8d11aa8b2afef7
minsik <minsik1382@naver.com>
2026-06-25 15:35:22 +0900

    Docs: 기능추가"

--- ops/multiply.py
+++ ops/multiply.py
@@ -1,5 +1,19 @@
 def multiply(a, b):
     # TODO: 두 수를 곱해 반환하세요.
+    """_summary_
+    숫자가 아닌 값이 입력으로 들어오면 None을 반환합니다.
+
+    Args:
+        a (_type_): _description_
+        b (_type_): _description_
+
+    Raises:
+        ValueError: _description_
+
+    Returns:
+        _type_: INT | FLOAT | None
+    """
+
     if not isinstance(a, (int, float)) or not isinstance(b, (int, float)):
         raise ValueError("Both a and b must be numbers.")
     return a * b
