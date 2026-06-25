9530ac121779b1421874fbc4de40e72fdef53558
beepcon <skkwon0801@gmail.com>
2026-06-25 11:47:18 +0900

    Feat: subtract 연산 추가

--- calculator.py
+++ calculator.py
@@ -2,6 +2,4 @@ from ops.add import add
 from ops.divide import divide
 from ops.subtract import subtract
 
-operations = {"+": add}
-operations = {"/": divide}
-operations = {"-": subtract}
+operations = {"+": add , "-": subtract, "/": divide}
