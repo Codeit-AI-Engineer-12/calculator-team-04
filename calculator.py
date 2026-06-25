bd7f117807866055b9988cdc3796b3c0c6ea7076
kdh <zldnfrjdi@naver.com>
2026-06-25 17:22:05 +0900

    Reapply "Feat: 최솟값 기능 설명 추가"
    This reverts commit ba599493f40c678d7d56c6e01c8f82ec94644bd5.

--- calculator.py
+++ calculator.py
@@ -2,10 +2,12 @@ from ops.add import add
 from ops.divide import divide
 from ops.subtract import subtract
 from ops.multiply import multiply
+from ops.min import min
 
 operations = {
     "+": add ,
     "-": subtract, 
     "/": divide, 
-    "*": multiply
+    "*": multiply,
+    "min": min
     }
