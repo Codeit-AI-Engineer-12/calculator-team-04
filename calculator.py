024207ead462d63826bf96704af77f50d8766fda
kdh <zldnfrjdi@naver.com>
2026-06-25 18:13:03 +0900

    Feat: 최솟값 기능 설명 추가

--- calculator.py
+++ calculator.py
@@ -1,13 +1,7 @@
 from ops.add import add
 from ops.divide import divide
-from ops.subtract import subtract
-from ops.multiply import multiply
 from ops.min import min
+from ops.multiply import multiply
+from ops.subtract import subtract
 
-operations = {
-    "+": add ,
-    "-": subtract, 
-    "/": divide, 
-    "*": multiply,
-    "min": min
-    }
+operations = {"+": add, "-": subtract, "/": divide, "*": multiply, "min": min}
