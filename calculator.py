41fc6179a0360de7a6a25bf7cab9438808802995
hyeoksang kwon <gurtkd3380@naver.com>
2026-06-26 09:22:58 +0900

    Feat: 나머지 나눗셈 기능 구현 / 리드미,메인 소스 머지

--- calculator.py
+++ calculator.py
@@ -4,6 +4,7 @@ from ops.integer_division import integer_division
 from ops.multiply import multiply
 from ops.root import root
 from ops.subtract import subtract
+from ops.modulo import modulo   
 
 operations = {
     "+": add,
@@ -12,4 +13,5 @@ operations = {
     "*": multiply,
     "//": integer_division,
     "**": root,
+    "%": modulo 
 }
